08f0f45cf3a21c2c5c411249ba1e2f1cb83a71ad
weiwucs <weiwusmile6@gmail.com>
2022-09-20 00:28:53 +0800

    commit

--- src/main/java/com/example/backend/entity/User.java
+++ src/main/java/com/example/backend/entity/User.java
@@ -1,10 +1,14 @@
 package com.example.backend.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
+@Getter
+@Setter
 @Entity
-@Table(name="user")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,36 +23,4 @@ public class User implements Serializable {
 
     @Column(nullable = false, length = 64)
     private String password;
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
